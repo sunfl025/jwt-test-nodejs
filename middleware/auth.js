e0d80f925fd9e7c7dc5f7f31514fb40f2ca540f2
sunfl025 <dttrang0205@gmail.com>
2023-08-04 01:21:19 +0700

    ha

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,21 +1,108 @@
 const jwt = require("jsonwebtoken");
-
+const bcrypt = require('bcryptjs')
 const config = process.env;
+const User =require("../model/user");
+const authController = {
+  //MIDDLEWARE
+    verifyToken : (req, res, next) => {
+    const token =
+      req.body.token || req.query.token || req.headers["x-access-token"];
+  
+    if (!token) {
+      return res.status(403).send("A token is required for authentication");
+    }
+    try {
+      const decoded = jwt.verify(token, config.TOKEN_KEY);
+      req.user = decoded;
+    } catch (err) {
+      return res.status(401).send("Invalid Token");
+    }
+    return next();
+  },
+  register : async(req,res) => {
+    // our register logic goes here...
+    try {
+      // Get user input
+      const { first_name, last_name, email, password } = req.body;
 
-const verifyToken = (req, res, next) => {
-  const token =
-    req.body.token || req.query.token || req.headers["x-access-token"];
+      // Validate user input
+      if (!(email && password && first_name && last_name)) {
+        res.status(400).json({status: "All input is required"});
+      }
 
-  if (!token) {
-    return res.status(403).send("A token is required for authentication");
+      // check if user already exist
+      // Validate if user exist in our database
+      
+      const oldUser = await User.findOne({ email });
+
+      if (oldUser) {
+        return res.status(409).json({message:"User Already Exist. Please Login"});
+      }
+
+      //Encrypt user password
+      encryptedPassword = await bcrypt.hash(password, 10);
+
+      // Create user in our database
+      const user = await User.create({
+        first_name,
+        last_name,
+        email: email.toLowerCase(), // sanitize: convert email to lowercase
+        password: encryptedPassword,
+      });
+
+      // Create token
+      const token = jwt.sign(
+        { user_id: user._id, email },
+        process.env.TOKEN_KEY,
+        {
+          expiresIn: "2h",
+        }
+      );
+      // save user token
+      user.token = token;
+
+      // return new user
+      res.status(201).json(user);
+    } catch (err) {
+      console.log(err);
+    }
+
+  },
+  login : async(req,res) => {
+      try {
+        // Get user input
+        const { email, password } = req.body;
+    
+        // Validate user input
+        if (!(email && password)) {
+          res.status(400).send("All input is required");
+        }
+        // Validate if user exist in our database
+        const user = await User.findOne({ email });
+    
+        if (user && (await bcrypt.compare(password, user.password))) {
+          // Create token
+          const token = jwt.sign(
+            { user_id: user._id, email },
+            process.env.TOKEN_KEY,
+            {
+              expiresIn: "2h",
+            }
+          );
+    
+          // save user token
+          user.token = token;
+    
+          // user
+          res.status(200).json(user);
+        }
+        res.status(400).send("Invalid Credentials");
+      } catch (err) {
+        console.log(err);
+      }
   }
-  try {
-    const decoded = jwt.verify(token, config.TOKEN_KEY);
-    req.user = decoded;
-  } catch (err) {
-    return res.status(401).send("Invalid Token");
-  }
-  return next();
-};
 
-module.exports = verifyToken;
+}
+   
+
+module.exports = authController;
---
+++ routes/auth.js
@@ -0,0 +1,11 @@
+const authController = require("../middleware/auth");
+
+const router = require("express").Router();
+
+router.post("/register",authController.register);
+router.get("/login",authController.login);
+
+router.post("/welcome", authController.verifyToken, (req, res) => {
+    res.status(200).send("Welcome 🙌 ");
+  });
+ module.exports = router;
